3cda30f25a6414928aa4afc999b67891771ec3af
Eduardo Pavarino <pavarinodu@gmail.com>
2016-09-13 08:44:08 -0300

    Done selection and classification methods

--- libs/darwin.py
+++ libs/darwin.py
@@ -1,4 +1,7 @@
 import numpy as np
+from sklearn import tree, linear_model, svm
+from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB
+#
 from ... import definitions
 from . import gene_handler as gnh, ClassificationModule as clfModule, constants, file_manager_module as fmm, array_handler, dicts
 from FeatureSelectionMethods.T_Statistics import T_Statistics
@@ -12,6 +15,7 @@ class Darwin(object):
 		self.initializePopulation(populationSize)
 
 
+	# Cria a população inicial
 	def initializePopulation(self, populationSize):
 		for p in range(populationSize):
 			newIndividual = dict(dicts.gene)
@@ -25,6 +29,8 @@ class Darwin(object):
 			self.population.append(newIndividual)
 
 
+	# Seleciona as características de acordo com o método de seleção
+	# apontado pelo gene do indivíduo e retorna os índices selecionados
 	def select(self, individual):
 		(data, labels) = fmm.getInputDataFromFile(individual['trainingFile'])
 
@@ -34,9 +40,29 @@ class Darwin(object):
 			return array_handler.getBestIndexes(t_statistics.getResult(), individual['numberOfSelectedFeatures'])
 
 
-	def classify(self, individual):
-		
+	# Classifica de acordo com o método de classificação
+	# apontado pelo gene do indivíduo e retorna a acurácia obtida
+	def classify(self, individual, training_data, training_labels, test_data, test_labels):
+		if(individual['classificationMethod'] == constants.DecisionTree):
+			clf = tree.DecisionTreeClassifier()
+		elif(individual['classificationMethod'] == constants.Lasso):
+			clf = linear_model.Lasso(alpha = 0.1)
+		elif(individual['classificationMethod']  == constants.SVM):
+			clf = svm.SVC()
+		elif(individual['classificationMethod']  == constants.GaussianNB):
+			clf = GaussianNB()
+		elif(individual['classificationMethod']  == constants.MultinomialNB):
+			clf = MultinomialNB()
+		elif(individual['classificationMethod']  == constants.BernoulliNB):
+			clf = BernoulliNB()
+		else:
+			raise Exception("Invalid classifier!")
+			exit()
+
+		clf.fit(training_data, training_labels)
+		return clf.score(test_data, test_labels)
 
 
 	def evaluate(self):
-		
+		for individual in self.population:
+			
